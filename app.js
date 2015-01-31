600d9a85f76ed48fdb17de6615022e2a58809b24
vicanso <vicansocanbico@gmail.com>
2015-01-31 16:12:49 +0800

    commit

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ jtLogger.appPath = __dirname + '/';
 var config = require('./config');
 var Client = require('./lib/client');
 var _ = require('lodash');
+var bytes = require('bytes');
 
 var JTStatsClient = require('jtstats_client');
 var jtStatsClient = new JTStatsClient({
   uri : config.statsUri,
   category : config.category
 });
+
+
+var memoryLog = function(){
+  var memoryUsage = process.memoryUsage();
+  var rss = bytes(memoryUsage.rss);
+  var heapTotal = bytes(memoryUsage.heapTotal);
+  var heapUsed = bytes(memoryUsage.heapUsed);
+  
+  console.info('memory.rss.%s', rss);
+  console.info('memory.heapTotal.%s', heapTotal);
+  console.info('memory.heapUsed.%s', heapUsed);
+};
+
 
 var client = new Client(config.mongodbUri, jtStatsClient);
 
@@ -18,6 +32,7 @@ var doStats = function(){
   client.doStats(function(){
     _.delay(doStats, interval);
   });
+  memoryLog();
 };
 
 doStats();
